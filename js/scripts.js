5fbac56492f431e4d54e61a12a5a78a2c6f4f33c
Newton Bii <biinewton382@gmail.com>
2017-11-06 21:52:05 +0300

    Add styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $("#toggle").click(function(event){
   event.preventDefault();
   $(".sidebar").fadeToggle(1000);
+  $("#toggle").hide(500);
+});
+
+$("#x-button").click(function(){
+  $(".sidebar").slideUp(1000);
+  $("#toggle").show(500);
 });
