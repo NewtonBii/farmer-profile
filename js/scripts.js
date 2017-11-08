560a1178f2c19d1dbb7404d77cc05b8bc8164014
Newton Bii <biinewton382@gmail.com>
2017-11-08 15:29:41 +0300

    Add button features.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $("#button1").click(function(event) {
   event.preventDefault();
   $(".cotn_principal").slideDown(1000);
+  $("#google-plus").show(100);
+  $("#instagram").show(300);
+  $("#linkedin").show(500);
+  $("#twitter").show(700);
+  $("#facebook").show(900);
+});
+
+$(".close").click(function(event){
+  $(".cotn_principal").slideUp(500);
 });
