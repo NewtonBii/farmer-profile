6738ac3758ce284c51aa889157140dfc267ee99a
Newton Bii <biinewton382@gmail.com>
2017-11-08 12:09:22 +0300

    Add login form.

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,8 @@ $("#x-button").click(function(){
   $("#sidebar1").slideUp(500);
   $("#toggle").show(1000);
 });
+
+$("#button1").click(function(event){
+  event.preventDefault();
+  window.location.replace("buyer.html");
+});
